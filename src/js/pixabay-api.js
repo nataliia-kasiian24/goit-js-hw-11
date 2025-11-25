57ff9c03a705e930ec7f8d3901f2f400f9b5f300
Kasiian_Nataliia <natalakasian@gmail.com>
2025-11-25 23:25:02 +0200

    add http

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,6 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const API_KEY = '53410313-8865f85a69864d0c0c63fb400';
+const BASE_URL = 'https://pixabay.com/api/';
+
